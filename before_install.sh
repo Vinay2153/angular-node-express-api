7483778bc4dbee87c68537c19a2bcb7419db898a
Vinay2153 <105987390+Vinay2153@users.noreply.github.com>
2022-09-08 13:44:52 +0530

    Create before_install.sh

---
+++ before_install.sh
@@ -0,0 +1,33 @@
+
+#!/bin/bash
+
+
+
+cd /home/ec2-user/server
+
+
+
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+
+
+#nvm install --lts
+
+
+
+#curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
+
+
+
+
+
+
+
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+
+
+
+yum -y install nodejs npm
+
+
+
